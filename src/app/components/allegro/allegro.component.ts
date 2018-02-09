645fa06ff3b77031e39d3b75e5f107f60281e24f
Kamil Różański <kgrozanski@gmail.com>
2018-02-09 04:22:08 +0100

    1.6.4
    - Added working get for Allegro Version Key

--- src/app/components/allegro/allegro.component.ts
+++ src/app/components/allegro/allegro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
 
 declare var $: any;
 @Component({
@@ -8,7 +10,38 @@ declare var $: any;
   styleUrls: ['./allegro.component.css']
 })
 
-export class AllegroComponent {
+export class AllegroComponent implements OnInit {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: Http) {}
+
+
+  ngOnInit() {
+    $(document).ready(function() {
+      console.log();
+    });
+  }
+
+
+  getVerKey() {
+    const headerss = new Headers({
+      'Accept': 'text/plain'
+    });
+    return this.http.get('http://api.allerest.pl/doQueryAllSysStatus?countryId=1&webapiKey=495ecfc0', {headers: headerss});
+  }
+  onGet() {
+    this.getVerKey().subscribe(
+      (response) => {
+        const keyArray = response.json();
+        console.log(keyArray);
+        var array = keyArray['sysCountryStatus']['item'][0]['verKey'];
+        console.log(array);
+      },
+      (error) => console.log(error)
+    );
+
+   
+
+
+    
+  }
 }
