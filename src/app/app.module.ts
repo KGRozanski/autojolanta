45c947b984f69bb88fc7c776d98ac25c8e910592
KGRozanski <kgrozanski@gmail.com>
2023-02-18 22:44:41 +0100

    Init v.15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentComponent } from './components/content/content.component';
-import { MapComponent } from './components/map/map.component';
-import { HttpModule } from '@angular/http';
-import { AgmCoreModule } from '@agm/core';
-
-import { FormsModule } from '@angular/forms';
-import { AllegroComponent } from './components/allegro/allegro.component';
-
-
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContentComponent,
-    MapComponent,
-    AllegroComponent
-  ],
-  imports: [
-    BrowserModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDCHUX5vXuJYzdHlwiVvYpUqc24uoE5B1o'
-    }),
-    FormsModule,
-    HttpModule
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
